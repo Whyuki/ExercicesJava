15dfb652a4692f056b87700e8481dc0c9076ca4c
Joanna <joanna.schmitt@outlook.fr>
2019-12-16 09:35:41 +0100

    commit

---
+++ TestJava/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ *
+ *//**
+ * @author CRM
+ *
+ */
+public class HelloWorld {	/**
+	 * @param args argument
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !!!");
+		}
+	}
