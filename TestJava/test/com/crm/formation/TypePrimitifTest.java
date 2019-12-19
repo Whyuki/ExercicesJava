81e571aed5eb878c9a257433a58166416e0f4862
Joanna <joanna.schmitt@outlook.fr>
2019-12-19 12:22:31 +0100

    test diff

--- TestJava/test/com/crm/formation/TypePrimitifTest.java
+++ TestJava/test/com/crm/formation/TypePrimitifTest.java
@@ -14,6 +14,12 @@ public class TypePrimitifTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int age, age2, diff;
+		age = 28;
+		age2 = 24;
+		diff = 0;
+		diff = age - age2;
+		System.out.println("La différence d'âge entre "+age+" ans et "+age2+" ans est de "+diff+" ans");		
 		
 	}
 
