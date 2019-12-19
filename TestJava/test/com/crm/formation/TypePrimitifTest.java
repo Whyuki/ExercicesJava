50784cb8addf712b97fde46266f482dc03758854
Joanna <joanna.schmitt@outlook.fr>
2019-12-19 09:04:14 +0100

    type primitif

---
+++ TestJava/test/com/crm/formation/TypePrimitifTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class TypePrimitifTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
