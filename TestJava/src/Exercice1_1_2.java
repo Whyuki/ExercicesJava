b2cb2dd8fa3b66a15513903b4bec1029228700dd
Joanna <joanna.schmitt@outlook.fr>
2020-01-06 10:55:05 +0100

    exo1.1.2

---
+++ TestJava/src/Exercice1_1_2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class Exercice1_1_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int premierNombre, secondNombre;
+		float moyenne;
+		boolean réponse;
+		Scanner sc;
+		sc = new Scanner(System.in);
+
+		réponse = true;
+		while (réponse == true) {
+			System.out.println("Veuillez saisir un premier nombre :");
+			premierNombre = sc.nextInt();
+			System.out.println("Veuillez saisir un second nombre :");
+			secondNombre = sc.nextInt();
+			moyenne = (premierNombre + secondNombre) / 2f;
+			System.out.println("La moyenne de ces deux nombres est de : " + moyenne);
+			System.out.println("Souhaitez vous faire un autre calcul ? si oui tapez 'true', sinon: tapez 'false'");
+			réponse = sc.nextBoolean();
+		}
+		sc.close();
+	}
+
+}
---
+++ TestJava/src/Exercice1_1_3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class Exercice1_1_3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
