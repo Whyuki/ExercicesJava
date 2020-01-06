9a5057eb4ab2c315ed520d2dff6c604ee9980d66
Joanna <joanna.schmitt@outlook.fr>
2020-01-06 09:57:20 +0100

    perimetre rectangle

---
+++ TestJava/src/PerimetreRectangle.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class PerimetreRectangle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc;
+		sc = new Scanner(System.in);
+		float longueurRectangle, largeurRectangle, perimetreRectangle;
+		System.out.println("Veuillez saisir la longueur du rectangle ");
+		longueurRectangle = sc.nextFloat();
+		System.out.println("Veuillez saisir la largeur du rectangle");
+		largeurRectangle = sc.nextFloat();
+		perimetreRectangle = (longueurRectangle + largeurRectangle)*2f;
+		System.out.println("Longueur du rectangle : "+longueurRectangle+"cm \nLargeur du rectangle : "+largeurRectangle+"cm \nPérimètre du rectangle : "+perimetreRectangle+"cm");
+		sc.close();
+		
+	}
+
+}
--- TestJava/test/com/crm/formation/OpérateursTest.java
+++ TestJava/test/com/crm/formation/OpérateursTest.java
@@ -15,7 +15,7 @@ public class OpérateursTest {
 	public static void main(String[] args) {
 		
 		int maVariableInt1;
-		maVariableInt1 = 36; // opérateur d'affectation : =
+		maVariableInt1 = 36; // opérateur d'affectation (ou d'assignement) : =
 		System.out.println(maVariableInt1);
 		
 		int maVariableInt2;
@@ -23,10 +23,46 @@ public class OpérateursTest {
 		System.out.println(maVariableInt2);
 		
 		int maVariableInt3;
-		maVariableInt3 = maVariableInt2 = maVariableInt1 = 58;
+		maVariableInt3 = maVariableInt2 = maVariableInt1 = 58; // assignement de droite à gauche
 		System.out.println(maVariableInt1);
 		System.out.println(maVariableInt2);
 		System.out.println(maVariableInt3);
+		
+		int somme1;
+		somme1 = 5 + 6;
+		System.out.println(somme1);
+		
+		int somme2;
+		somme2 = somme1 + 7;
+		System.out.println(somme2);
+		
+		int sous1;
+		sous1 = 5-2;
+		System.out.println(sous1);
+		
+		int prod1;
+		prod1 = 5*3;
+		System.out.println(prod1);
+		
+		float produit2;
+		produit2 = 5.2f*3;
+		System.out.println(produit2);
+		
+		float quotient1;
+		quotient1 = 4.3f/3.2f;
+		System.out.println(quotient1);
+		
+		int quotient2;
+		quotient2 = 5/2;
+		System.out.println(quotient2); //arrondi
+		
+		float quotient3;
+		quotient3 = 5f/2; // f !!!
+		System.out.println(quotient3);
+		
+		int modulo1;
+		modulo1 = 5 % 2;
+		System.out.println(modulo1);
 		
 		
 		
