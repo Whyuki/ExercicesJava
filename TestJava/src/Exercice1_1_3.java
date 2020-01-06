64ab9e7a74cd76861087aeb2b54acd45db32cceb
Joanna <joanna.schmitt@outlook.fr>
2020-01-06 15:45:10 +0100

    test compteur

--- TestJava/src/Exercice1_1_3.java
+++ TestJava/src/Exercice1_1_3.java
@@ -15,6 +15,27 @@ public class Exercice1_1_3 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int nombre, compteurDeNombre, somme;
+		float moyenne;
+		Scanner sc;
+		sc = new Scanner(System.in);
+		compteurDeNombre = 0;
+		somme = 0;
+		nombre =0;
+		System.out.println("Veuillez saisir un nombre :");
+		nombre = sc.nextInt();
+		somme = somme + nombre;
+		compteurDeNombre++;
+		while (nombre > 0) {
+			System.out.println("Veuillez saisir un autre nombre :");
+			nombre = sc.nextInt();
+			somme = somme + nombre;
+			compteurDeNombre++;
+			moyenne = somme / compteurDeNombre;
+			System.out.println("somme : " + somme);
+			System.out.println("compteur de nombre : " + compteurDeNombre);
+			System.out.println("moyenne : " + moyenne);
+		}
+		sc.close();
 	}
 }
