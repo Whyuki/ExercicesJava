18551c637b879d9a36801e908645208e4c8f93b2
Joanna <joanna.schmitt@outlook.fr>
2020-01-07 16:50:14 +0100

    1.2

---
+++ TestJava/src/Exercice1_2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class Exercice1_2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
