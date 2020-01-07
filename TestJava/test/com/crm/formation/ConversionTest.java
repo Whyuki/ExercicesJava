6c7207d8e9fe50655ae0999bec9620f8e0af5e8c
Joanna <joanna.schmitt@outlook.fr>
2020-01-07 09:42:58 +0100

    increm decrem

--- TestJava/test/com/crm/formation/ConversionTest.java
+++ TestJava/test/com/crm/formation/ConversionTest.java
@@ -18,13 +18,23 @@ public class ConversionTest {
 		long y = x;
 		System.out.println("x : "+x+"\ny : "+y);
 
-//conversion implicite
+					//conversion implicite
 		
 		int a = 65537;
 		short z = (short)a;
 		System.out.println("a : "+a+"\nz : "+z);
-//conversion explicite
+					//conversion explicite
 
+		long l1 = 123456789;
+		long l2 = 123456788;
+		float f1 = l1;
+		float f2 = l2;
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(l1 - l2);
+		System.out.println(f1 - f2);
+
+										// perte de précision entrainant une erreur
 	}
 
 }
--- TestJava/test/com/crm/formation/OpérateursTest.java
+++ TestJava/test/com/crm/formation/OpérateursTest.java
@@ -94,7 +94,33 @@ public class OpérateursTest {
 		num %= pourcentage;
 		System.out.println("\ntotal :"+total);
 		System.out.println("num :"+num);
-		System.out.println("pourcentage :"+pourcentage);
+		System.out.println("pourcentage :"+pourcentage+"\n");
+		
+		
+		
+		int var1, var2;
+		var1 = 3;
+		var2 = 0;
+		System.out.println("var1 = " + var1 + "\nvar2 = " + var2);
+		var2 = ++var1;
+		System.out.println("préfixe ++ : var2 = ++var1 donne: \n" + "var1 = " + var1 + "\nvar2 = " + var2); // incrémentation
+																											// avant
+																											// (ré)affectation
+		var1 = 3;
+		var2 = 0;
+		System.out.println("Remise à zéro\n" + "var1 = " + var1 + "\nvar2 = " + var2);
+		var2 = var1++;
+		System.out.println("suffixe ++ : var2 = var1++ donne: \n" + "var1 = " + var1 + "\nvar2 = " + var2); // incrémentation
+																											// apres
+																											// (ré)affectation
+
+		
+		
+		
+		int var3;
+		var3 = 8;
+		var3--;
+		System.out.println("\n"+"var3 = "+var3);
 		
 	}
 
