e80ca2ebcaaf2110d789223200af0efc414b0865
Joanna <joanna.schmitt@outlook.fr>
2020-01-08 11:17:48 +0100

    comment

--- TestJava/src/Exercice1_1_3.java
+++ TestJava/src/Exercice1_1_3.java
@@ -14,7 +14,7 @@ public class Exercice1_1_3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO seulement additionner et calculer la moyenne qu'à la fin ?
 		int nombre, compteurDeNombre, somme;
 		float moyenne;
 		boolean rep;
