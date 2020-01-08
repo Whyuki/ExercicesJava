aba3973a81a9f3e679a7aa9d16c8406d7c39b25f
Joanna <joanna.schmitt@outlook.fr>
2020-01-08 08:38:00 +0100

    format

--- TestJava/src/com/crm/api/Utils.java
+++ TestJava/src/com/crm/api/Utils.java
@@ -3,7 +3,6 @@
  */
 package com.crm.api;
 
-
 /**
  * @author CRM
  *
@@ -12,8 +11,8 @@ public class Utils {
 
 	public static float somme(float a, float b) {
 
-		float s = (float)a + (float)b;
-		return s;			 
+		float s = (float) a + (float) b;
+		return s;
 	}
 
 	public static float perimeterRectangle(float length, float width) {
