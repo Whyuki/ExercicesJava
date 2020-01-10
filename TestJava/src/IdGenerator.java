b963982ebcf60c2c415a0fdb2fe8814d0c3429fc
Joanna <joanna.schmitt@outlook.fr>
2020-01-10 08:55:13 +0100

    idgen

--- TestJava/src/IdGenerator.java
+++ TestJava/src/IdGenerator.java
@@ -15,20 +15,21 @@ public class IdGenerator {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
-		Scanner sc = new Scanner (System.in);
+
+		Scanner sc = new Scanner(System.in);
 		String firstName, name, login, passworld;
 		System.out.println("Please enter your firstname");
 		firstName = sc.nextLine();
 		System.out.println("Please enter your name");
 		name = sc.nextLine();
-		login = firstName.substring(0, 1)+"."+name.toUpperCase();
-		passworld = name.substring(0, 1)+name.substring(1, 2).toUpperCase()+name.substring(2).replace('a', '@')+firstName.length();
-		System.out.println("Your login is : "+login);
-		System.out.println("Your passworld is :"+passworld);
-				
+		login = firstName.substring(0, 1) + "." + name.toUpperCase();
+		passworld = name.substring(0, 1) + name.substring(1, 2).toUpperCase() + name.substring(2).replace('a', '@')
+				+ firstName.length();
+		System.out.println("Your login is : " + login);
+		System.out.println("Your passworld is :" + passworld);
+
 		sc.close();
-		
+
 	}
 
 }
