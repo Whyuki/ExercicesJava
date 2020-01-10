e8a387e79903e9c69fe6509e9b01be1c7a7c8069
joann <joann@192.168.202.36>
2020-01-10 15:34:30 +0100

    optimisation test reda

--- TestJava/src/IdGenerator.java
+++ TestJava/src/IdGenerator.java
@@ -23,7 +23,7 @@ public class IdGenerator {
 		System.out.println("Please enter your name");
 		name = sc.nextLine();
 		login = firstName.substring(0, 1) + "." + name.toUpperCase();
-		passworld = name.substring(0, 1) + name.substring(1, 2).toUpperCase() + name.substring(2).replace('a', '@').replace('m', 'w')
+		passworld = name.substring(0, 1) + name.substring(1, 2).replace('a', '@').replace('A', '@').replace('m', 'w').replace('M', 'W').toUpperCase() + name.substring(2).replace('a', '@').replace('A', '@').replace('m', 'w').replace('M', 'W')
 				+ firstName.length();
 		System.out.println("Your login is : " + login);
 		System.out.println("Your passworld is :" + passworld);
