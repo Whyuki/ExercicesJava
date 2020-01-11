0353eea1717064f58aa30004c728982fbc3c0c9f
joann <joann@DESKTOP-ON6LCCU>
2020-01-12 00:21:21 +0100

    3try/login

--- TestJava/src/IdCheckerApp.java
+++ TestJava/src/IdCheckerApp.java
@@ -32,15 +32,11 @@ public class IdCheckerApp {
 				System.out.println("remaining try: " + tryAllowed);
 			}
 			if (tryAllowed == 0) {
-				System.out.println("Incorrect login or password, try again :");
+				System.out.println("\nIncorrect login or password, try again :");
 				checkLogin = false;
 				checkPassword = false;
 				i = 0;
 				tryAllowed = 3;
-				System.out.println(checkLogin);
-				System.out.println(checkPassword);
-				System.out.println(tryAllowed);
-				System.out.println(i);
 				System.out.println("Please enter your login :");
 				login = sc.nextLine();
 				checkLogin = (login.contentEquals("J.SCHMITT")) || (login.contentEquals("JO"));
