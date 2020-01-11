6833769a69469940ca0cb7ab4ce29d0491f6df6d
joann <joann@192.168.202.36>
2020-01-11 18:51:46 +0100

    update

--- TestJava/src/IdChecker3try.java
+++ TestJava/src/IdChecker3try.java
@@ -20,23 +20,23 @@ public class IdChecker3try {
 		int i = 0, tryAllowed = 3;
 		boolean checkLogin = false, checkPassword = false;
 		while (checkLogin == false) {
-			System.out.println("Login :");
+			System.out.println("Please enter your login :");
 			login = sc.nextLine();
 			checkLogin = (login.contentEquals("J.SCHMITT"));
 
 		}
 		checkPassword = false;
 		while (checkPassword == false && i < 3) {
-			System.out.println("Password :" + "\n" + "Remaining try : " + tryAllowed);
+			System.out.println("Please enter your password :" + "\n" + "Remaining try : " + tryAllowed);
 			password = sc.nextLine();
 			checkPassword = (password.contentEquals("SChwitt6"));
 			i++;
 			tryAllowed--;
 		}
 		if (checkPassword == true) {
-			System.out.println("Connected");
+			System.out.println("You are connected");
 		} else {
-			System.out.println("error");
+			System.out.println("Remaining try : " + tryAllowed + "\nYour account is blocked");
 		}
 		sc.close();
 
