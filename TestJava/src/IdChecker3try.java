b42dc110dec03977649cab683bdb3ae11c4191de
joann <joann@192.168.202.36>
2020-01-11 17:36:18 +0100

    id checker with 3 try

---
+++ TestJava/src/IdChecker3try.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author joann
+ *
+ */
+public class IdChecker3try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String login, password;
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		boolean checkLogin = false, checkPassword = false;
+		System.out.println("Login :");
+		login = sc.nextLine();
+		checkLogin = (login.contentEquals("JOANNA"));
+		System.out.println(checkLogin);
+		while (checkLogin == false) {
+			System.out.println("incorrect login, try again :");
+			login = sc.next();
+			checkLogin = (login.contentEquals("JOANNA"));
+		}
+		System.out.println("log ok");
+		while (checkPassword == false && i < 3) {
+			System.out.println("Password :");
+			password = sc.nextLine();
+			checkPassword = (password.contentEquals("SChwitt6"));
+			i++;
+			// System.out.println("try " + i + "\nincorrect password, try again :");
+		}
+		if (checkPassword == true) {
+			System.out.println("Connected");
+		} else {
+			System.out.println("error");
+		}
+
+		sc.close();
+
+	}
+
+}
