26fb3a1f01af442d0bd598c174e847c9a0c4fffd
joann <joann@192.168.202.36>
2020-01-11 15:50:04 +0100

    OPTI IDGEN

--- TestJava/src/IdGenerator.java
+++ TestJava/src/IdGenerator.java
@@ -22,8 +22,8 @@ public class IdGenerator {
 		firstName = sc.nextLine();
 		System.out.println("Please enter your name");
 		name = sc.nextLine();
-		login = firstName.substring(0, 1) + "." + name.toUpperCase();
-		passworld = name.substring(0, 1) + name.substring(1, 2).replace('a', '@').replace('A', '@').replace('m', 'w').replace('M', 'W').toUpperCase() + name.substring(2).replace('a', '@').replace('A', '@').replace('m', 'w').replace('M', 'W')
+		login = firstName.substring(0, 1).toUpperCase() + "." + name.toUpperCase();
+		passworld = name.substring(0, 1) + name.substring(1, 2).toUpperCase().replace('A', '@').replace('M', 'W') + name.substring(2).toLowerCase().replace('a', '@').replace('m', 'w')
 				+ firstName.length();
 		System.out.println("Your login is : " + login);
 		System.out.println("Your passworld is :" + passworld);
