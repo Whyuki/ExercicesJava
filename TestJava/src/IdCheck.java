7edf37ce7ffd9176e15b238a0a3384395aed4cbf
joann <joann@192.168.202.36>
2020-01-11 16:21:30 +0100

    ID checker

---
+++ TestJava/src/IdCheck.java
@@ -0,0 +1,38 @@
+import java.security.DrbgParameters.NextBytes;
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author joann
+ *
+ */
+public class IdCheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String login, password;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Login :");
+		login = sc.nextLine();
+		if (login.equals("J.SCHMITT")) {
+			System.out.println("Password :");
+			password = sc.nextLine();
+			if (password.equals("SChwitt6")) { // !!! string == "blabla" ---> str.equals("blabla")
+				System.out.println("Connected");
+			} else {
+				System.out.println("incorrect password");
+			}
+		} else {
+			System.out.println("incorrect login");
+		}
+		sc.close();
+
+	}
+
+}
