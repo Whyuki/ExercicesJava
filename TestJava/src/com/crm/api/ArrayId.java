4099037835f7758a77b6e19159863d8b4a94a173
joann <joann@DESKTOP-ON6LCCU>
2020-01-12 02:37:05 +0100

    pause

--- TestJava/src/com/crm/api/ArrayId.java
+++ TestJava/src/com/crm/api/ArrayId.java
@@ -9,13 +9,13 @@ package com.crm.api;
  */
 public class ArrayId {
 
-	public static final String LOGIN_ARRAY[] = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET", "D.DOGHMANE",
-			"R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY", "N.THOUVENOT",
-			"S.TORRENTI" };
+	public static final String LOGIN_ARRAY[] = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
+			"D.DOGHMANE", "R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY",
+			"N.THOUVENOT", "S.TORRENTI" };
 
+	public static final String PASSWORD_ARRAY[] = { "A.B", "T.B", "L.C", "K.C", "G.C", "D.D", "R.H", "J.J", "M.R",
+			"J.S", "B.S", "L.S", "N.T", "S.T" };
 
-public static final String PASSWORD_ARRAY[] = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET", "D.DOGHMANE",
-		"R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY", "N.THOUVENOT",
-		"S.TORRENTI" };
-
+	
+	// do a method for match login/pwd
 }
--- TestJava/src/com/crm/api/ArrayTest.java
+++ TestJava/src/com/crm/api/ArrayTest.java
@@ -19,17 +19,17 @@ public class ArrayTest {
 //		String[] loginT = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET", "D.DOGHMANE",
 //				"R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY", "N.THOUVENOT",
 //				"S.TORRENTI" };
-		String str;
+		String login, pwd;
 		int posi;
 		boolean check = false;
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("login");
-		str = sc.next();
+		login = sc.next();
 
 		for (int i = 0; i < ArrayId.LOGIN_ARRAY.length; i++) {
 
-			if (str.contentEquals(ArrayId.LOGIN_ARRAY[i])) {
+			if (login.contentEquals(ArrayId.LOGIN_ARRAY[i])) {
 				posi = i; //for check password
 				check = true;
 				System.out.println("position "+posi);
@@ -42,6 +42,17 @@ public class ArrayTest {
 
 			System.out.println("rip" + " it's " + check);
 		}
+		System.out.println("pwd :");
+		pwd = sc.next();
+//posi= pwd
+//		if (pwd.contentEquals(ArrayId.PASSWORD_ARRAY[posi])) {
+//			System.out.println("oki" + " it's " + check);
+//		} else {
+//
+//			System.out.println("rip");
+//		}
+
+		
 		sc.close();
 	}
 }
