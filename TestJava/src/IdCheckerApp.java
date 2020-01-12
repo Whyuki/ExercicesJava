def9eba5f2999025423cf463b94d856634140e29
joann <joann@DESKTOP-ON6LCCU>
2020-01-12 15:08:32 +0100

    1ok

--- TestJava/src/IdCheckerApp.java
+++ TestJava/src/IdCheckerApp.java
@@ -21,26 +21,22 @@ public class IdCheckerApp {
 		Scanner sc = new Scanner(System.in);
 		int i = 0, tryAllowed = 3, posi = -1;
 		boolean checkLogin = false, checkPassword = false;
-		System.out.println("Please enter your login :");
-		login = sc.nextLine();
-		for (int ix = 0; ix < ArrayId.LOGIN_ARRAY.length; ix++) {
 
-			if (login.contentEquals(ArrayId.LOGIN_ARRAY[ix])) {
-				posi = ix;
-				break;
-			}
-		}
-		if (posi >= 0) {
-			checkLogin = true;
-			System.out.println(" it's ok "+posi);
-//			checkLogin = (posi >= 0);
-		} else {
-			System.out.println("rip");
-		}
-		while (checkPassword == false && i < 3) {
+		while (checkLogin == false && checkPassword == false && i < 3) {
+			System.out.println("Please enter your login :");
+			login = sc.nextLine();
 			System.out.println("Please enter your password :");
 			password = sc.nextLine();
+			for (int ix = 0; ix < ArrayId.LOGIN_ARRAY.length; ix++) {
+
+				if (login.contentEquals(ArrayId.LOGIN_ARRAY[ix])) {
+					posi = ix;
+					break;
+				}
+			}
+			checkLogin = (posi >= 0);
 			checkPassword = (password.contentEquals(ArrayId.PASSWORD_ARRAY[posi]));
+			System.out.println(posi);
 			i++;
 			tryAllowed--;
 			if (checkLogin == false || checkPassword == false) {
@@ -52,9 +48,6 @@ public class IdCheckerApp {
 				checkPassword = false;
 				i = 0;
 				tryAllowed = 3;
-				System.out.println("Please enter your login :");
-				login = sc.nextLine();
-				checkLogin = (login.contentEquals("J.SCHMITT")) || (login.contentEquals("JO"));
 			}
 		}
 		if (checkLogin == true && checkPassword == true) {
