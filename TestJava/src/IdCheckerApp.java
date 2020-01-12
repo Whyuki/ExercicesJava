2dcb305b66e239d659d967e78c4aa92b471338e8
joann <joann@DESKTOP-ON6LCCU>
2020-01-12 14:50:46 +0100

    array id

--- TestJava/src/IdCheckerApp.java
+++ TestJava/src/IdCheckerApp.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import com.crm.api.ArrayId;
+
 /**
  * 
  */
@@ -17,15 +19,28 @@ public class IdCheckerApp {
 		// TODO Auto-generated method stub
 		String login, password;
 		Scanner sc = new Scanner(System.in);
-		int i = 0, tryAllowed = 3;
+		int i = 0, tryAllowed = 3, posi = -1;
 		boolean checkLogin = false, checkPassword = false;
 		System.out.println("Please enter your login :");
 		login = sc.nextLine();
-		checkLogin = (login.contentEquals("J.SCHMITT")) || (login.contentEquals("JO"));
+		for (int ix = 0; ix < ArrayId.LOGIN_ARRAY.length; ix++) {
+
+			if (login.contentEquals(ArrayId.LOGIN_ARRAY[ix])) {
+				posi = ix;
+				break;
+			}
+		}
+		if (posi >= 0) {
+			checkLogin = true;
+			System.out.println(" it's ok "+posi);
+//			checkLogin = (posi >= 0);
+		} else {
+			System.out.println("rip");
+		}
 		while (checkPassword == false && i < 3) {
 			System.out.println("Please enter your password :");
 			password = sc.nextLine();
-			checkPassword = (password.contentEquals("666"));
+			checkPassword = (password.contentEquals(ArrayId.PASSWORD_ARRAY[posi]));
 			i++;
 			tryAllowed--;
 			if (checkLogin == false || checkPassword == false) {
