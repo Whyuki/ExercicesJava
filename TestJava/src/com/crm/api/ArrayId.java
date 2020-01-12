f2ea9f6c33f543f3d858f6a8c0409edd91d5fb29
joann <joann@DESKTOP-ON6LCCU>
2020-01-12 17:17:17 +0100

    final ?

--- TestJava/src/com/crm/api/ArrayId.java
+++ TestJava/src/com/crm/api/ArrayId.java
@@ -9,15 +9,15 @@ package com.crm.api;
  */
 public class ArrayId {
 
-	public static final String LOGIN_ARRAY[] = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
+	public static String LOGIN_ARRAY[] = { "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
 			"D.DOGHMANE", "R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY",
 			"N.THOUVENOT", "S.TORRENTI" };
 
-	public static final String PASSWORD_ARRAY[] = { "BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghm@n35", "H@mz@4", "J3@nni@rd9", "R@v@ndoust6",
+	public static String PASSWORD_ARRAY[] = { "BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghm@n35", "H@mz@4", "J3@nni@rd9", "R@v@ndoust6",
 			"SChwitt6", "SCho3nu@3k3r8", "S3dy7", "THouv3not7", "TOrr3nti7" };
 
 	
-	public static final String ID_ARRAY[][] = {{ "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
+	public static String ID_ARRAY[][] = {{ "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
 			"D.DOGHMANE", "R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY",
 			"N.THOUVENOT", "S.TORRENTI" },{"BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghm@n35", "H@mz@4", "J3@nni@rd9", "R@v@ndoust6",
 				"SChwitt6", "SCho3nu@3k3r8", "S3dy7", "THouv3not7", "TOrr3nti7"}};
