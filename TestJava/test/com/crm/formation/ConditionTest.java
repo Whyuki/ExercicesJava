47e034fb1cad1ab6db104cac8360878e14c37b23
Joanna <joanna.schmitt@outlook.fr>
2020-01-13 09:12:16 +0100

    if else

---
+++ TestJava/test/com/crm/formation/ConditionTest.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+/*
+ * 
+ * if (estVrai) System.out.println("YES"); (une seule instruction accolades facultatives)
+ */
+public class ConditionTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean estVrai = false;
+		System.out.println("Début programme");
+
+		if (estVrai) { // if (estVrai == true){
+			System.out.println("YES");
+			System.out.println("WE CAN");
+		} else {
+			System.out.println("NO");
+			System.out.println("WE CAN'T");
+		}
+
+		System.out.println("Fin de programme");
+	}
+
+}
