bebb35aa081a561420d012d76bbc44784259aaff
Joanna <joanna.schmitt@outlook.fr>
2020-01-13 08:10:32 +0100

    fix pwd

--- TestJava/src/IdGenerator.java
+++ TestJava/src/IdGenerator.java
@@ -24,7 +24,7 @@ public class IdGenerator {
 		name = sc.nextLine();
 		login = firstName.substring(0, 1).toUpperCase() + "." + name.toUpperCase();
 		passworld = name.substring(0, 2).toUpperCase().replace('A', '@').replace('M', 'W').replace('E', '3').replace('W', 'U')
-				+ name.substring(2).toLowerCase().replace('a', '@').replace('m', 'w').replace('e', '3').replace('w', 'u') + firstName.length();
+				+ name.substring(2).toLowerCase().replace('a', '@').replace('m', 'p').replace('e', '3').replace('w', 'u') + firstName.length();
 		System.out.println("Your login is : " + login);
 		System.out.println("Your passworld is :" + passworld);
 		sc.close();
