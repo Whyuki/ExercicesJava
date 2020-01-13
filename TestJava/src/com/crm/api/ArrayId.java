50a299378ca1c1a6e938323f3ae36d5285f912e6
joann <joann@DESKTOP-ON6LCCU>
2020-01-13 17:15:50 +0100

    fix array

--- TestJava/src/com/crm/api/ArrayId.java
+++ TestJava/src/com/crm/api/ArrayId.java
@@ -13,13 +13,9 @@ public class ArrayId {
 			"D.DOGHMANE", "R.HAMZA", "J.JEANNIARD", "M.RAVANDOUST", "J.SCHMITT", "B.SCHOENMAEKER", "L.SEDY",
 			"N.THOUVENOT", "S.TORRENTI" };
 
-<<<<<<< HEAD
-	public static final String PASSWORD_ARRAY[] = { "BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghm@n35", "H@mz@4", "J3@nni@rd9", "R@v@ndoust6",
-			"SChwitt6", "SCho3nu@3k3r8", "S3dy7", "THouv3not7", "TOrr3nti7" };
-=======
+
 	public static final String PASSWORD_ARRAY[] = { "BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghp@n35", "H@pz@4", "J3@nni@rd9", "R@v@ndoust6",
 			"SChpitt6", "SCho3nu@3k3r8", "S3dy7", "THouv3not7", "TOrr3nti7" };
->>>>>>> branch 'master' of https://github.com/Whyuki/ExercicesJava.git
 
 	
 	public static final String ID_ARRAY[][] = {{ "A.BOUDIER", "T.BROWN", "L.CARRIVALE", "K.CHARIH", "G.CROUZET",
