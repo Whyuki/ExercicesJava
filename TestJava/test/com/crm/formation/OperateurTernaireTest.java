7f037262109c90de6898f60d463eb36a124b781e
Joanna <joanna.schmitt@outlook.fr>
2020-01-14 09:54:39 +0100

    ternary operator

---
+++ TestJava/test/com/crm/formation/OperateurTernaireTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class OperateurTernaireTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// ternary operator
+		
+		int nb1 =4, nb2 = 3;
+		boolean isTrue = nb1 > nb2 ? true : false;
+		System.out.println(isTrue);
+		
+	}
+
+}
