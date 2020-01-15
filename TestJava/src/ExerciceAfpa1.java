8fa86c499693cc7a0fd94bc8729177ec83f89b12
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 09:38:37 +0100

    afpa exo1

---
+++ TestJava/src/ExerciceAfpa1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class ExerciceAfpa1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, i = 0, somme = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre :");
+		n = sc.nextInt();
+		while (i <= n) {
+			somme = somme + i;
+			i++;
+		}
+		System.out.println("La somme des nombres de 0 à " + n + " est de " + somme);
+		sc.close();
+	}
+
+}
