8c3957b866fede18268162f4f23b3dd2b7c6ef62
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 16:50:52 +0100

    nbTentative

---
+++ TestJava/src/DessineMoi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class DessineMoi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i = 0, longu;
+		char star ='*';
+		System.out.println("Entrez la longueur");
+		longu = sc.nextInt();
+		while (i < longu) {
+			System.out.println(star+star);
+			i++;
+		}
+
+		sc.close();
+	}
+
+}
--- TestJava/src/DevineUnNombre.java
+++ TestJava/src/DevineUnNombre.java
@@ -15,22 +15,22 @@ public class DevineUnNombre {
 	 */
 	public static void main(String[] args) {
 		// TODO idée nombre de tentative max ? game over ?
-		int n = 0, devi = 64, i = 0;
+		int n = 0, devi = 64, nbTentative = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
 		while (n != devi) {
 			n = sc.nextInt();
-			i++;
+			nbTentative++;
 			if (n > 100) {
 				System.out.println("Nombre suppérieur à 100, réessayez : ");
 			} else if (n < 0) {
 				System.out.println("Nombre inférieur à 0, réessayez : ");
 			} else if (n == devi) {
 				System.out.println("Le bon nombre est bien " + devi);
-				if (i < 2) {
-					System.out.println("Vous l'avez trouvé en " + i + " essai");
+				if (nbTentative < 2) {
+					System.out.println("Vous l'avez trouvé en " + nbTentative + " essai");
 				} else {
-					System.out.println("Vous l'avez trouvé en " + i + " essais");
+					System.out.println("Vous l'avez trouvé en " + nbTentative + " essais");
 				}
 			} else if (n > devi) {
 				System.out.println("C'est moins, réessayez :");
