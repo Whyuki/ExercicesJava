73d0305b12aff87e9460767afbf9bd731844c7f8
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 11:20:58 +0100

    exo 3 & 4 afpa

---
+++ TestJava/src/ExerciceAfpa4.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class ExerciceAfpa4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x, i = 0, y, resultat;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre :");
+		x = sc.nextInt();
+		System.out.println("Veuillez saisir son exposant :");
+		y = sc.nextInt();
+		resultat = Math.pow(x, y);
+		System.out.println("résulat : " + resultat);
+		sc.close();
+	}
+
+}
