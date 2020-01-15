a4cd5aa6c4f199a618426e3f127205cb0f883e8d
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 11:52:08 +0100

    devinette v2

--- TestJava/src/DevineUnNombre.java
+++ TestJava/src/DevineUnNombre.java
@@ -15,20 +15,37 @@ public class DevineUnNombre {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		int n = 0, devi = 64;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
+//		while (n >= 0 && n <= 100) {
+//			n = sc.nextInt();
+//			if (n == devi) {
+//				System.out.println("Le bon nombre est bien " + devi);
+//			} else if (n > devi) {
+//				System.out.println("C'est moins, réessayez :");
+//			} else {
+//				System.out.println("C'est plus, réessayez :");
+//			}
+//
+//		}
+//		sc.close();
+
 		int n = 0, devi = 64;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
-		while (n != devi) {
-			n = sc.nextInt();
-			if (n > devi) {
-				System.out.println("C'est moins, réessayez :");
-			} else {
-				System.out.println("C'est plus, réessayez :");
+		n = sc.nextInt();
+		if (n == devi) {
+			System.out.println("Le bon nombre est bien " + devi);
+		} else
+			while (n != devi) {
+				if (n > devi) {
+					System.out.println("C'est moins, réessayez :");
+				} else {
+					System.out.println("C'est plus, réessayez :");
+				}
+
 			}
-		}
-
 		sc.close();
-
 	}
-
 }
