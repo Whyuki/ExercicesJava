458238d3064d296d1821f4a4188d05a9c475ddf5
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 12:07:51 +0100

    devi

--- TestJava/src/DevineUnNombre.java
+++ TestJava/src/DevineUnNombre.java
@@ -15,22 +15,51 @@ public class DevineUnNombre {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 0, devi = 64;
+//		int n = 0, devi = 64, i = 0;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
+//		while (n >= 0) {
+//			n = sc.nextInt();
+//			i++;
+//			if (n > 100) {
+//				System.out.println("Nombre suppérieur à 100, réessayez : ");
+//			} else if (n == devi) {
+//				System.out.println("Le bon nombre est bien " + devi);
+//				if (i < 2) {
+//					System.out.println("Vous l'avez trouvé en " + i + " essai");
+//				} else {
+//					System.out.println("Vous l'avez trouvé en " + i + " essais");
+//				}
+//			} else if (n > devi) {
+//				System.out.println("C'est moins, réessayez :");
+//			} else {
+//				System.out.println("C'est plus, réessayez :");
+//			}
+//		}
+//		sc.close();
+		
+		int n = 0, devi = 64, i = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
-		while (n >= 0 && n <= 100) {
+		while (n != devi) {
 			n = sc.nextInt();
-			if (n == devi) {
-				System.out.println("Le bon nombre est bien " + devi);
+			i++;
+			if (n > 100) {
+				System.out.println("Nombre suppérieur à 100, réessayez : ");
 			} else if (n > devi) {
 				System.out.println("C'est moins, réessayez :");
 			} else {
 				System.out.println("C'est plus, réessayez :");
 			}
-
+		}
+		if (n == devi) {
+			System.out.println("Le bon nombre est bien " + devi);
+			if (i < 2) {
+				System.out.println("Vous l'avez trouvé en " + i + " essai");
+			} else {
+				System.out.println("Vous l'avez trouvé en " + i + " essais");
+			}
 		}
 		sc.close();
-
-
 	}
 }
