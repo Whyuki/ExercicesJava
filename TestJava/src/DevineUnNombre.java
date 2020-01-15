3aaa8b8e77f2ec73a0323a561524bdc3f7ba658f
Joanna <joanna.schmitt@outlook.fr>
2020-01-15 12:21:29 +0100

    devinette

--- TestJava/src/DevineUnNombre.java
+++ TestJava/src/DevineUnNombre.java
@@ -15,51 +15,28 @@ public class DevineUnNombre {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int n = 0, devi = 64, i = 0;
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
-//		while (n >= 0) {
-//			n = sc.nextInt();
-//			i++;
-//			if (n > 100) {
-//				System.out.println("Nombre suppérieur à 100, réessayez : ");
-//			} else if (n == devi) {
-//				System.out.println("Le bon nombre est bien " + devi);
-//				if (i < 2) {
-//					System.out.println("Vous l'avez trouvé en " + i + " essai");
-//				} else {
-//					System.out.println("Vous l'avez trouvé en " + i + " essais");
-//				}
-//			} else if (n > devi) {
-//				System.out.println("C'est moins, réessayez :");
-//			} else {
-//				System.out.println("C'est plus, réessayez :");
-//			}
-//		}
-//		sc.close();
-		
 		int n = 0, devi = 64, i = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
-		while (n != devi) {
+		while (n >= 0) {
 			n = sc.nextInt();
 			i++;
 			if (n > 100) {
 				System.out.println("Nombre suppérieur à 100, réessayez : ");
+			} else if (n == devi) {
+				System.out.println("Le bon nombre est bien " + devi);
+				if (i < 2) {
+					System.out.println("Vous l'avez trouvé en " + i + " essai");
+				} else {
+					System.out.println("Vous l'avez trouvé en " + i + " essais");
+				}
 			} else if (n > devi) {
 				System.out.println("C'est moins, réessayez :");
 			} else {
 				System.out.println("C'est plus, réessayez :");
 			}
 		}
-		if (n == devi) {
-			System.out.println("Le bon nombre est bien " + devi);
-			if (i < 2) {
-				System.out.println("Vous l'avez trouvé en " + i + " essai");
-			} else {
-				System.out.println("Vous l'avez trouvé en " + i + " essais");
-			}
-		}
 		sc.close();
+
 	}
 }
