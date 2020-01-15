37139db89c76ee19e63c04c770fc5d55f19221b3
joann <joann@192.168.202.107>
2020-01-15 21:45:12 +0100

    hasNextInt

---
+++ TestJava/test/com/crm/formation/HasNextTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author joann
+ *
+ */
+public class HasNextTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 0, devi = 64, nbTentative = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
+		while (n != devi) {
+//			System.out.println(sc.hasNextInt());
+			if (sc.hasNextInt()) {
+				n = sc.nextInt();
+				nbTentative++;
+				if (n > 100) {
+					System.out.println("Nombre suppérieur à 100, réessayez : ");
+				} else if (n < 0) {
+					System.out.println("Nombre inférieur à 0, réessayez : ");
+				} else if (n == devi) {
+					System.out.println("Le bon nombre est bien " + devi);
+					if (nbTentative < 2) {
+						System.out.println("Vous l'avez trouvé en " + nbTentative + " essai");
+					} else {
+						System.out.println("Vous l'avez trouvé en " + nbTentative + " essais");
+					}
+				} else if (n > devi) {
+					System.out.println("C'est moins, réessayez :");
+				} else {
+					System.out.println("C'est plus, réessayez :");
+				}
+			} else {
+				sc.next();
+				System.out.println("Ceci n'est pas un nombre ! Réessayez :");
+			}
+		}
+
+		sc.close();
+	}
+}
