fc63675b68087d4d0f4916a7ff4b76b4b14d2e4a
Joanna <joanna.schmitt@outlook.fr>
2020-01-16 10:38:16 +0100

    random 0 100 math

---
+++ TestJava/test/com/crm/formation/RandomTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+/**
+ * @author CRM
+ *
+ */
+public class RandomTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// generation random avec Math.random entre 0 et 100 ?
+		int r = (int) (Math.random() * 1000000) % 100;
+		System.out.println(r);
+	}
+
+}
