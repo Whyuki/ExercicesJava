d3254f1476e98b40ef788b8061fe5abf8893bbe6
Joanna <joanna.schmitt@outlook.fr>
2020-01-16 08:12:23 +0100

    todo idea

--- TestJava/src/DevineUnNombre.java
+++ TestJava/src/DevineUnNombre.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,29 +16,38 @@ public class DevineUnNombre {
 	 */
 	public static void main(String[] args) {
 		// TODO idée nombre de tentative max ? game over ?
-		int n = 0, devi = 64, nbTentative = 0;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
-		while (n != devi) {
-			n = sc.nextInt();
-			nbTentative++;
-			if (n > 100) {
-				System.out.println("Nombre suppérieur à 100, réessayez : ");
-			} else if (n < 0) {
-				System.out.println("Nombre inférieur à 0, réessayez : ");
-			} else if (n == devi) {
-				System.out.println("Le bon nombre est bien " + devi);
-				if (nbTentative < 2) {
-					System.out.println("Vous l'avez trouvé en " + nbTentative + " essai");
-				} else {
-					System.out.println("Vous l'avez trouvé en " + nbTentative + " essais");
+		// TODO fix nombre de tentative en cas de mauvaise saisie ?
+				int n = 0, devi = 0, nbTentative = 0;
+				int r = new Random().nextInt(101);
+				Scanner sc = new Scanner(System.in);
+				devi = r;
+				System.out.println("Veuillez saisir un nombre compris entre 0 et 100 :");
+				while (n != devi) {
+//					System.out.println(sc.hasNextInt());
+					if (sc.hasNextInt()) {
+						n = sc.nextInt();
+						nbTentative++;
+						if (n > 100) {
+							System.out.println("Nombre suppérieur à 100, réessayez : ");
+						} else if (n < 0) {
+							System.out.println("Nombre inférieur à 0, réessayez : ");
+						} else if (n == devi) {
+							System.out.println("Le bon nombre est bien " + devi);
+							if (nbTentative < 2) {
+								System.out.println("Vous l'avez trouvé en " + nbTentative + " essai");
+							} else {
+								System.out.println("Vous l'avez trouvé en " + nbTentative + " essais");
+							}
+						} else if (n > devi) {
+							System.out.println("C'est moins, réessayez :");
+						} else {
+							System.out.println("C'est plus, réessayez :");
+						}
+					} else {
+						sc.next();
+						System.out.println("Ceci n'est pas un nombre ! Réessayez :");
+					}
 				}
-			} else if (n > devi) {
-				System.out.println("C'est moins, réessayez :");
-			} else {
-				System.out.println("C'est plus, réessayez :");
-			}
-		}
-		sc.close();
+				sc.close();
 	}
 }
