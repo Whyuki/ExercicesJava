cb1d7f22716f23f8fed07c69d8c396c1febbb1b0
Joanna <joanna.schmitt@outlook.fr>
2020-01-16 11:55:08 +0100

    commentaire break, continue, return

--- TestJava/test/com/crm/formation/InstructionReturnTest.java
+++ TestJava/test/com/crm/formation/InstructionReturnTest.java
@@ -16,15 +16,16 @@ public class InstructionReturnTest { // class
 		// TODO Auto-generated method stub
 		int nombre = 10;
 		afficherListeNombre(nombre);
+
 	}
 
 	public static void afficherListeNombre(int nb) { // méthode afficherListeNombre
 		System.out.println("Début méthode afficherListeNombre");
 		for (int i = 0; i < nb; i++) {
 			if (i == 5) {
-//				break;
-//				continue;      //continue la boucle = ignore si la condition est remplie
-				return; // quitte/ferme la méthode : donc n'exécute pas la suite des instructions ..."
+				break;					  //quitte la boucle et exécute la suite des instruction de la méthode
+//				continue;     			 //continue la boucle = ignore si la condition est remplie
+//				return; 				// quitte/ferme la méthode : donc n'exécute pas la suite des instructions ..."
 			}
 			System.out.println(i);
 		}
