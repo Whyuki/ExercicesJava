467d6fe472ae140c4d27e9d491a68edcd6c419ce
Joanna <joanna.schmitt@outlook.fr>
2020-01-16 08:32:25 +0100

    to do idea enumeration

--- TestJava/src/SwitchOrientation.java
+++ TestJava/src/SwitchOrientation.java
@@ -14,7 +14,7 @@ public class SwitchOrientation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO énumération à voir
 		String choix = "ok";
 		Scanner sc = new Scanner(System.in);
 		final String CDA = "Concepteur Développeur d'Application", DWWM = " Développeur Web Web-Mobile", REORIENTATION = "réorentation professionnelle", EN_COURS_DE_REFLEXION = "en cours de réflexion";
--- TestJava/test/com/crm/formation/BoucleForTest.java
+++ TestJava/test/com/crm/formation/BoucleForTest.java
@@ -14,10 +14,19 @@ public class BoucleForTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i = 999;
-		for (i = 0; i < 10; i++) {
-			System.out.println("i = " + i);
+//		int i = 999;
+//		for (i = 0; i < 10; i++) {
+//			System.out.println("i = " + i);
+//		}
+//		System.out.println("after for : i = " + i); // déclaration i avant boucle for sinon i inaccessible
+
+		for (int x = -5; x <= 5; x++) {
+			if (x == 0) {
+				System.out.println("division par 0");
+				break;
+			}
+			System.out.println(1.0 / x);
 		}
-		System.out.println("after for : i = " + i); // déclaration i avant boucle for sinon i inaccessible
+
 	}
 }
