496ac5ce3a811d46ca48810093760930dc8e9871
Joanna <joanna.schmitt@outlook.fr>
2020-01-20 09:43:22 +0100

    tableaux

--- TestJava/test/com/crm/formation/TableauTest.java
+++ TestJava/test/com/crm/formation/TableauTest.java
@@ -14,9 +14,53 @@ public class TableauTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		// Déclaration de tableau :
+		// 1ere méthode déclarative :
+		int[] tablInt = new int[5];
+		// 2nde méthode déclarative :
+		int tablInt1[] = new int[5];
+
+		// Déclaration et initialisation d'un tableau
+		int[] tablInt3 = { 1, 2, 3 };
+
+		// Accès à un élément du tableau
+		int premierElement, deuxiemeElement, troisiemeElement;
+//		int quatriemeElement;
+		premierElement = tablInt3[0];
+		System.out.println("Premier élément (indice 0) : " + premierElement);
+		deuxiemeElement = tablInt3[1];
+		System.out.println("Deuxième élément (indice 1) : " + deuxiemeElement);
+		troisiemeElement = tablInt3[2];
+		System.out.println("Troisième élément (indice 2) : " + troisiemeElement);
+//		quatriemeElement = tablInt3[3];
+//		System.out.println("Quatrième élément : " + quatriemeElement);
+		/*
+		 * Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 3 at
+		 * com.crm.formation.TableauTest.main(TableauTest.java:33)
+		 * 
+		 */
+
+		// Modification valeur d'un élément du tableau
+		tablInt3[0] = 10;
+		premierElement = tablInt3[0];
+		System.out.println("Premier élément (après modification) : " + premierElement);
+
+		// Afficher la taille du tableau
+		System.out.println("Taille du tableau tablInt3 : " + tablInt3.length);
+
+		// Parcours du tableau :
+		System.out.println(tablInt3[0]);
+		System.out.println(tablInt3[1]);
+		System.out.println(tablInt3[2]);
+		// Parcours avec une boucle :
+		for (int i = 0; i < tablInt3.length; i++) {
+			System.out.println(tablInt3[i]);
+		}
+
 		int[] tabInt;
 		tabInt = new int[5];
-		System.out.println("longeur tabInt" + tabInt.length);
+		System.out.println("\nlongeur tabInt : " + tabInt.length);
 
 		int[] tabInt1 = new int[5];
 		System.out.println(tabInt1[0]);
