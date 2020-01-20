c70d93c90edc34d1570187b9eef01c97dfbd5e4a
joann <joann@DESKTOP-ON6LCCU>
2020-01-20 23:09:59 +0100

    dice roll

--- TestJava/src/com/crm/api/Dice.java
+++ TestJava/src/com/crm/api/Dice.java
@@ -21,17 +21,48 @@ public class Dice {
 		 * deux joueurs, 3 lancés de dès, si 6 rejouer, somme pour déterminer gagnant
 		 */
 		Scanner sc = new Scanner(System.in);
-		String player1, player2;
-		int dice = 0;
-		System.out.println("Player 1, please enter your pseudo :");
-		player1 = sc.nextLine();
-		System.out.println("Player 2, please enter your pseudo :");
-		player2 = sc.nextLine();
-		System.out.println("Player 1, please roll the dice :");
-		// range random 0 inclus !!!
-		dice = new Random().nextInt(7);
-		System.out.println(dice);
 
+		String pseudo[] = new String[2];
+		int score[] = new int[2];
+		int dice = 0, nbJet = 0;
+
+		System.out.println("Player 1, please enter your pseudo :");
+		pseudo[0] = sc.nextLine();
+		System.out.println("Player 2, please enter your pseudo :");
+		pseudo[1] = sc.nextLine();
+
+		for (int i = 0; i < pseudo.length; i++) {
+
+			System.out.println(pseudo[i] + ", please roll the dice :");
+
+			while (nbJet < 3) {
+
+				nbJet++;
+				dice = new Random().nextInt(7);
+				if (dice > 0) {
+					System.out.println("Jet : " + dice);
+				}
+				score[i] = score[i] + dice;
+				if (dice == 6 || dice == 0) {
+					nbJet--;
+				}
+
+			}
+
+			System.out.println(pseudo[i] + "'s score : " + score[i]);
+
+			nbJet = 0;
+			dice = 0;
+
+		}
+
+		if (score[0] > score[1]) {
+			System.out.println(pseudo[0] + " win !");
+		} else if (score[0] == score[1]) {
+			System.out.println("=");
+		} else {
+			System.out.println(pseudo[1] + " win !");
+		}
 	}
 
 }
