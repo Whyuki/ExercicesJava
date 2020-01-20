34be7810ed460ea3681361aa611b4839c5bc0aab
joann <joann@DESKTOP-ON6LCCU>
2020-01-20 18:14:47 +0100

    paladromeTest à opti, mais fonctionne

--- TestJava/src/com/crm/api/Palindrome.java
+++ TestJava/src/com/crm/api/Palindrome.java
@@ -17,31 +17,22 @@ public class Palindrome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String answer;
-		boolean palindrome = false;
+		String motSaisi = "", motInverse = "";
+		char c;
 
 		System.out.println("Veuillez saisir un mot : ");
-		answer = sc.nextLine();
+		motSaisi = sc.nextLine();
 
-		for (int i = 0, j = answer.toCharArray().length - 1; i < j; i++, j--) {
-//			System.out.println(i);
-//			System.out.println(j);
-			if (answer.charAt(i) != answer.charAt(j)) {
-				System.out.println("Ce mot n'est pas un palindrome");
-				palindrome = false;
-				break;
-//			System.out.println(answer.charAt(i));
-//			System.out.println(answer.charAt(j));
-//				
-			} else {
-				palindrome = true;
-				continue;
-				
-			}
+		for (int i = motSaisi.toCharArray().length - 1; i >= 0; i--) {
+			c = motSaisi.charAt(i);
+			motInverse = motInverse + c;
 
 		}
-		if (palindrome == true) {
-			System.out.println("Ce mot est un palindrome.");
+
+		if (motSaisi.equals(motInverse)) {
+			System.out.println("Ce mot est un palindrome");
+		} else {
+			System.out.println("Ce mot n'est pas un palindrome");
 		}
 
 		sc.close();
---
+++ TestJava/test/com/crm/formation/PalindromeTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.crm.formation;
+
+import java.util.Scanner;
+
+/**
+ * @author joann
+ *
+ */
+public class PalindromeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String answer;
+		boolean palindrome = false;
+
+		System.out.println("Veuillez saisir un mot : ");
+		answer = sc.nextLine();
+
+		for (int i = 0, j = answer.toCharArray().length - 1; i < j; i++, j--) {
+			if (answer.charAt(i) != answer.charAt(j)) {
+				System.out.println("Ce mot n'est pas un palindrome");
+				palindrome = false;
+				break;
+			} else {
+				palindrome = true;
+				continue;
+			}
+		}
+		if (palindrome == true) {
+			System.out.println("Ce mot est un palindrome.");
+		}
+
+		sc.close();
+	}
+
+}
