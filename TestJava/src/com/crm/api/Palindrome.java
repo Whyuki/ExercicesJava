65c959a3c0100384f785d8efcd3e81ce3bd700bb
Joanna <joanna.schmitt@outlook.fr>
2020-01-20 16:59:32 +0100

    palindrome à refaire car ne fonctionne pas sur mot à nombre de caractère impaire

---
+++ TestJava/src/com/crm/api/Palindrome.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+import java.util.Scanner;
+
+/**
+ * @author CRM
+ *
+ */
+public class Palindrome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String answer;
+		boolean palindrome = false;
+
+		System.out.println("Veuillez saisir un mot : ");
+		answer = sc.nextLine();
+
+		for (int i = 0, j = answer.toCharArray().length - 1; i < j; i++, j--) {
+//			System.out.println(i);
+//			System.out.println(j);
+			if (answer.charAt(i) != answer.charAt(j)) {
+				System.out.println("Ce mot n'est pas un palindrome");
+				palindrome = false;
+				break;
+//			System.out.println(answer.charAt(i));
+//			System.out.println(answer.charAt(j));
+//				
+			} else {
+				palindrome = true;
+				continue;
+			}
+
+		}
+		if (palindrome == true) {
+			System.out.println("Ce mot est un palindrome.");
+		}
+
+		sc.close();
+	}
+
+}
--- TestJava/src/com/crm/api/TableauSemaine.java
+++ TestJava/src/com/crm/api/TableauSemaine.java
@@ -17,19 +17,28 @@ public class TableauSemaine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		String semaine[] = { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+		String[] semaine = { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
 		String week[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 		String answer;
-//		System.out.println("\nFrançais : ");
-//		for (i = 0; i < semaine.length; i++) {
-//			System.out.println(semaine[i]);
-//		}
-//		System.out.println("\nEnglish : ");
-//		for (i = 0; i < week.length; i++) {
-//			System.out.println(week[i]);
-//		}
-
-		System.out.println("Veuillez saisir un jour de la semaine en français pour le traduire en anglais : ");
+		
+		System.out.println("Français : ");
+		for (int i = 0; i < semaine.length; i++) {
+			System.out.println(semaine[i]);
+		}
+		System.out.println("\nEnglish : ");
+		for (String nb : week) {
+			System.out.println(nb);
+		}
+		System.out.println("\nFrançais (à l'envers) : ");
+		for (int i = semaine.length-1; i >=0; i--) {
+			System.out.println(semaine[i]);
+		}
+		System.out.println("\nEnglish (reverse) : ");
+		for (int i = week.length-1; i >=0; i--) {
+			System.out.println(week[i]);
+		}
+		
+		System.out.println("\nVeuillez saisir un jour de la semaine en français pour le traduire en anglais : ");
 		answer = sc.nextLine();
 		for (int i = 0; i < semaine.length; i++) {
 			if (answer.equalsIgnoreCase(semaine[i])) {
