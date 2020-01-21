9ea3fe7c1c64bc9fdfb234d9cc6fe7e6f4b10700
Joanna <joanna.schmitt@outlook.fr>
2020-01-21 16:18:59 +0100

    commentaire

--- TestJava/test/com/crm/formation/InstructionReturnTest.java
+++ TestJava/test/com/crm/formation/InstructionReturnTest.java
@@ -23,9 +23,11 @@ public class InstructionReturnTest { // class
 		System.out.println("Début méthode afficherListeNombre");
 		for (int i = 0; i < nb; i++) {
 			if (i == 5) {
-				break;					  //quitte la boucle et exécute la suite des instruction de la method
-//				continue;     			 //continue la boucle = ignore si la condition est remplie
+				System.out.println("ok");
+//				break;					  //quitte la boucle et exécute la suite des instruction de la method
+				continue;     			 //continue la boucle = si la condition est remplie, ignore les instructions de la boucle et passe à l'étaration suivante
 //				return; 				// quitte/ferme la méthode : donc n'exécute pas la suite des instructions ..."
+			
 			}
 			System.out.println(i);
 		}
