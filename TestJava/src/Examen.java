ec81aa1cf4670c72acf472317f39bc94af5d3fe8
Joanna <joanna.schmitt@outlook.fr>
2020-01-22 15:34:30 +0100

    exament note if imbriqué

--- TestJava/src/Examen.java
+++ TestJava/src/Examen.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 /**
  * @author Joanna
+ * @version 1.0
  *
  */
 /** 
---
+++ TestJava/src/ExamenNote.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Joanna
+ * @version 2.0
+ *
+ */
+public class ExamenNote {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int note;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entrez votre note :");
+		note = sc.nextInt();
+
+		if (note >= 0 && note <= 20) {
+			if (note >= 8) {
+				if (note >= 10) {
+					if (note >= 12) {
+						if (note >= 14) {
+							if (note >= 16) {
+								System.out.println("Vous êtes admis avec la mention TB");
+
+							} else {
+								System.out.println("Vous êtes admis avec la mention B");
+							}
+
+						} else {
+							System.out.println("Vous êtes admis avec la mention AB");
+						}
+
+					} else {
+						System.out.println("Vous êtes admis");
+					}
+
+				} else {
+					System.out.println("Vous êtes éligible au rattrapage");
+				}
+			} else {
+				System.out.println("Vous êtes ajourné");
+			}
+
+		} else {
+			System.out.println("Note invalide");
+		}
+
+		sc.close();
+
+	}
+
+}
