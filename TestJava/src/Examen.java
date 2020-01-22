53230331905dafbf6ba4e34074b7de391def7d0b
Joanna <joanna.schmitt@outlook.fr>
2020-01-22 15:37:23 +0100

    note exam v1 ajout range de 0 à 20

--- TestJava/src/Examen.java
+++ TestJava/src/Examen.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * @version 1.0
  *
  */
-/** 
+/**
  * résultat examen
  */
 public class Examen {
@@ -22,26 +22,28 @@ public class Examen {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Entrez votre note :");
 		note = sc.nextInt();
+		if (note >= 0 && note <= 20) {
+			if (note >= 16) {
+				System.out.println("Vous êtes admis avec la mention TB");
 
-		if (note >= 16) {
-			System.out.println("Vous êtes admis avec la mention TB");
+			} else if (note >= 14) {
+				System.out.println("Vous êtes admis avec la mention B");
 
-		} else if (note >= 14) {
-			System.out.println("Vous êtes admis avec la mention B");
+			} else if (note >= 12) {
+				System.out.println("Vous êtes admis avec la mention AB");
 
-		} else if (note >= 12) {
-			System.out.println("Vous êtes admis avec la mention AB");
+			} else if (note >= 10) {
+				System.out.println("Vous êtes admis");
 
-		} else if (note >= 10) {
-			System.out.println("Vous êtes admis");
+			} else if (note >= 8 && note < 10) {
+				System.out.println("Vous êtes éligible au rattrapage");
 
-		} else if (note >= 8 && note < 10) {
-			System.out.println("Vous êtes éligible au rattrapage");
-
-		} else {
-			System.out.println("Vous êtes ajourné");
+			} else {
+				System.out.println("Vous êtes ajourné");
+			}
+		}else {
+			System.out.println("Note invalide");
 		}
-
 		sc.close();
 	}
 }
