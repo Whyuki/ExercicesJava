33718eb631aded7fc33526086b1f0cd4e779d45f
Joanna <joanna.schmitt@outlook.fr>
2020-01-27 15:53:52 +0100

    comparaison tableaux de primitif

---
+++ TestJava/src/com/crm/api/CompareTableauPrimitif.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.crm.api;
+
+import java.util.Arrays;
+
+/**
+ * @author Joanna
+ *
+ */
+public class CompareTableauPrimitif {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] tab1 = { 1, 2, 3 };
+		int[] tab2 = { 1, 2, 4 };
+		boolean idem;
+		idem = Arrays.equals(tab1, tab2);
+		System.out.println("Tableaux identiques ? : " + idem);
+
+	}
+
+}
