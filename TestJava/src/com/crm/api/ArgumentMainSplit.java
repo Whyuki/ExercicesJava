3cd0d9ab3312d2b98ae3020745a294f298f77571
Joanna <joanna.schmitt@outlook.fr>
2020-01-27 10:14:21 +0100

    argsplit

--- TestJava/src/com/crm/api/ArgumentMainSplit.java
+++ TestJava/src/com/crm/api/ArgumentMainSplit.java
@@ -15,8 +15,17 @@ public class ArgumentMainSplit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
+		String[] argument0 = args[0].split("=");
+		String[] argument1 = args[1].split("=");
 
+
+		if (args.length == 2) {
+			if (argument0[0].equals("prenom")) {		
+				System.out.println("Bienvenue " + argument0[1] + " " + argument1[1]);
+			} else {
+				System.out.println("Bienvenue " + argument1[1] + " " + argument0[1]);
+			}
+		}
 	}
 
 }
