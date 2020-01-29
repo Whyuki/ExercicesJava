86271e233bd24869834945c90a97bb0c6c1822da
Joanna <joanna.schmitt@outlook.fr>
2020-01-29 09:52:18 +0100

    traducteur avec méthode

--- TestJava/src/com/crm/api/WeekTranslate.java
+++ TestJava/src/com/crm/api/WeekTranslate.java
@@ -5,6 +5,8 @@ package com.crm.api;
 
 import java.util.Scanner;
 
+
+
 /**
  * @author Joanna
  *
@@ -23,6 +25,7 @@ public class WeekTranslate {
 		String answer, langu;
 		String onOff = "Y";
 		Scanner sc = new Scanner(System.in);
+		int index;
 
 		while (onOff.equalsIgnoreCase("Y") || onOff.equalsIgnoreCase("O") || onOff.equalsIgnoreCase("J")) {
 			System.out.println("FR/EN/DE ?");
@@ -30,34 +33,28 @@ public class WeekTranslate {
 			if (langu.equalsIgnoreCase("FR")) {
 				System.out.println("Veuillez saisir un jour de la semaine :");
 				answer = sc.nextLine();
-				for (int i = 0; i < semaineWeek[0].length; i++) {
-					if (answer.equalsIgnoreCase(semaineWeek[0][i])) {
-						System.out.println("English : " + semaineWeek[1][i]);
-						System.out.println("Deutsch : " + semaineWeek[2][i]);
-					}
-				}
+				index = foundInd(semaineWeek, answer);
+				System.out.println("English : " + semaineWeek[1][index]);
+				System.out.println("Deutsch : " + semaineWeek[2][index]);
+
 				System.out.println("\nContinuer ? O/N");
 				onOff = sc.nextLine();
 			} else if (langu.equalsIgnoreCase("EN")) {
 				System.out.println("Please enter a day of the week :");
 				answer = sc.nextLine();
-				for (int i = 0; i < semaineWeek[1].length; i++) {
-					if (answer.equalsIgnoreCase(semaineWeek[1][i])) {
-						System.out.println("Français : " + semaineWeek[0][i]);
-						System.out.println("Deutsch : " + semaineWeek[2][i]);
-					}
-				}
+				index = foundInd(semaineWeek, answer);
+				System.out.println("Français : " + semaineWeek[0][index]);
+				System.out.println("Deutsch : " + semaineWeek[2][index]);
+
 				System.out.println("\nAnother one ? Y/N");
 				onOff = sc.nextLine();
 			} else if (langu.equalsIgnoreCase("DE")) {
 				System.out.println("Bitte geben Sie einen Wochentag :");
 				answer = sc.nextLine();
-				for (int i = 0; i < semaineWeek[2].length; i++) {
-					if (answer.equalsIgnoreCase(semaineWeek[2][i])) {
-						System.out.println("Français : " + semaineWeek[0][i]);
-						System.out.println("English : " + semaineWeek[1][i]);
-					}
-				}
+				index = foundInd(semaineWeek, answer);
+				System.out.println("Français : " + semaineWeek[0][index]);
+				System.out.println("English : " + semaineWeek[1][index]);
+
 				System.out.println("\nFortsetzen ? J/N");
 				onOff = sc.nextLine();
 			} else {
@@ -66,6 +63,28 @@ public class WeekTranslate {
 		}
 		sc.close();
 
+	}
+
+	public static int foundInd(String[][] array, String str) {
+		int ind = -1;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				if (str.equalsIgnoreCase(array[i][j])) {
+					ind = j;
+				}
+			}
+		}
+		return ind;
+	}
+
+	public static void parcoursTableau2dim(String[][] array) {
+
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				System.out.println("élément [" + i + "][" + j + "] : " + array[i][j]);
+			}
+			System.out.println();
+		}
 	}
 
 }
