19b675d200b4e526b8dad9cee205f65def8b65ab
Joanna <joanna.schmitt@outlook.fr>
2020-01-29 16:15:02 +0100

    commentaire et format

--- TestJava/src/com/crm/api/WeekTranslate.java
+++ TestJava/src/com/crm/api/WeekTranslate.java
@@ -20,18 +20,23 @@ public class WeekTranslate {
 				{ "Français", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" },
 				{ "English", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" },
 				{ "Deutsch", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag" } };
+		final int INDEX_FR = 0;
+		final int INDEX_EN = 1;
+		final int INDEX_DE = 2;
 		String word, langu;
 		String onOff = "Y";
 		Scanner sc = new Scanner(System.in);
 		int index;
 
+		// permet à l'utilisateur d'éffectuer une autre traduction
 		while (onOff.equalsIgnoreCase("Y") || onOff.equalsIgnoreCase("O") || onOff.equalsIgnoreCase("J")) {
 			System.out.println("FR/EN/DE ?");
 			langu = sc.nextLine();
 			if (langu.equalsIgnoreCase("FR")) {
 				System.out.println("Veuillez saisir un jour de la semaine :");
 				word = sc.nextLine();
-				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, 0);
+				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, INDEX_FR); // recherche du mot dans le tableau FR et
+																				// retour de l'index
 				if (index >= 0) {
 					System.out.println("English : " + SEMAINE_WEEK_WOCHE[1][index]);
 					System.out.println("Deutsch : " + SEMAINE_WEEK_WOCHE[2][index]);
@@ -44,7 +49,7 @@ public class WeekTranslate {
 			} else if (langu.equalsIgnoreCase("EN")) {
 				System.out.println("Please enter a day of the week :");
 				word = sc.nextLine();
-				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, 1);
+				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, INDEX_EN);
 				if (index >= 0) {
 					System.out.println("Français : " + SEMAINE_WEEK_WOCHE[0][index]);
 					System.out.println("Deutsch : " + SEMAINE_WEEK_WOCHE[2][index]);
@@ -57,7 +62,7 @@ public class WeekTranslate {
 			} else if (langu.equalsIgnoreCase("DE")) {
 				System.out.println("Bitte geben Sie einen Wochentag :");
 				word = sc.nextLine();
-				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, 2);
+				index = Utils.foundIndex(SEMAINE_WEEK_WOCHE, word, INDEX_DE);
 				if (index >= 0) {
 
 					System.out.println("Français : " + SEMAINE_WEEK_WOCHE[0][index]);
