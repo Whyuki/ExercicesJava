ec63795c4c482d0c1227def2ba769ae87144e62c
Joanna <joanna.schmitt@outlook.fr>
2020-01-31 08:26:25 +0100

    light & comments

--- TestJava/src/com/crm/api/Authentification.java
+++ TestJava/src/com/crm/api/Authentification.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 /**
  * @author Joanna
  *
- *         connexion avec trois tentatives par login, 
- *         demande login / demande password/ verification match
+ *         connexion avec trois tentatives par login
+ *         demande login / demande password / verification match 
  *         tableau deux dimensions contenant les login et password
  * 
  */
@@ -28,32 +28,37 @@ public class Authentification {
 				{ "BOudi3r8", "BRoun7", "C@rriv@l38", "CH@rih6", "CRouz3t7", "DOghp@n35", "H@pz@4", "J3@nni@rd9",
 						"R@v@ndoust6", "SChpitt6", "SCho3nu@3k3r8", "S3dy7", "THouv3not7", "TOrr3nti7" } };
 
-		String login, password;
+		String login = "", password = "";
 		Scanner sc = new Scanner(System.in);
 		final int INDEX_LOGIN = 0, INDEX_PASSWORD = 1;
 		int tryAllowed = 3, posi = -1;
 		boolean checkLogin = false, checkPassword = false;
 
-		System.out.println("Please enter your login :");
-		login = sc.nextLine();
 		while (checkLogin == false || checkPassword == false && tryAllowed >= 0) {
+			if (tryAllowed == 3) {
+				System.out.println("Please enter your login :");
+				login = sc.nextLine();
+			}
 			System.out.println("Please enter your password :");
 			password = sc.nextLine();
 			tryAllowed--;
-			posi = Utils.foundIndex(ID_TABL, login, INDEX_LOGIN); // recherche du login saisi dans le tableau login et affecte son indice à la variable posi
+			posi = Utils.foundIndex(ID_TABL, login, INDEX_LOGIN); // recherche du login saisi dans le tableau login et
+																	// affecte son indice à la variable posi
 			if (posi > 0) { // si le login est contenu dans le tableau login (posi est donc >0)
 				checkLogin = true;
-				checkPassword = (password.contentEquals(ID_TABL[INDEX_PASSWORD][posi])); // vérifie que le password saisi correspond au password lié au login saisi
+				checkPassword = (password.contentEquals(ID_TABL[INDEX_PASSWORD][posi])); // vérifie que le password
+																							// saisi correspond au
+																							// password lié au login
+																							// saisi
 			}
 			if (checkPassword == false) {
 				System.out.println("/!\\ Remaining try: " + tryAllowed);
-				if (tryAllowed == 0) { // après 3 tentative, si couple login/pwd mauvais : remise par défaut des variables et retour en début de boucle
+				if (tryAllowed == 0) { // après 3 tentative, si couple login/pwd mauvais : remise par défaut des
+										// variables et retour en début de boucle
 					checkLogin = false;
 					tryAllowed = 3;
 					posi = -1;
-					System.out.println("____________________________" + "\nIncorrect login or password, try again :"
-							+ "\nPlease enter your login :");
-					login = sc.nextLine();
+					System.out.println("____________________________" + "\nIncorrect login or password =[ "+"\nTry again.");
 				}
 			}
 		}
