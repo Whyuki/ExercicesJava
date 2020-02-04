77a5537c8e42e5e6b45545cb8adf673149642ae6
Joanna <joanna.schmitt@outlook.fr>
2020-02-04 16:10:02 +0100

    bug scanner next() instead nextLine()

--- TestJava/src/com/crm/boapp/TableauPersonneApp.java
+++ TestJava/src/com/crm/boapp/TableauPersonneApp.java
@@ -19,18 +19,17 @@ public class TableauPersonneApp {
 		int nbPersonne = 0;
 		System.out.println("De combien de personne se compose votre équipe ?");
 
-//		nbPersonne = sc.nextInt(); // -------------> bug : fix with Integer.parseInt(sc.nextLine()) thx Aurelien
-		nbPersonne = Integer.parseInt(sc.nextLine());
+		nbPersonne = sc.nextInt();
 		Personne[] tabPers = new Personne[nbPersonne]; // Déclaration d'un tableau d'objet de type Personne 
 		// construction d'objets de type Personne pour chaque case du tableau
 		for (int i = 0; i < tabPers.length; i++) {
 			tabPers[i] = new Personne();
 			System.out.println("\nMembre n° " + (i + 1) + ", veuillez saisir votre Prénom :");
-			tabPers[i].setPrenom(sc.nextLine());
+			tabPers[i].setPrenom(sc.next());
 			System.out.println("Veuillez saisir votre Nom :");
-			tabPers[i].setNom(sc.nextLine());
+			tabPers[i].setNom(sc.next());
 			System.out.println("Veuillez saisir votre Surnom :");
-			tabPers[i].surnom = sc.nextLine();
+			tabPers[i].surnom = sc.next();
 
 			tabPers[i].afficherDetails();
 			
