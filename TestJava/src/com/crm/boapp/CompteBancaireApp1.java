d8ddcb1b6ccc0d03c6d1de8279bf934287a82683
Joanna <joanna.schmitt@outlook.fr>
2020-02-06 15:23:50 +0100

    compte bancaire

--- TestJava/src/com/crm/boapp/CompteBancaireApp1.java
+++ TestJava/src/com/crm/boapp/CompteBancaireApp1.java
@@ -20,12 +20,15 @@ public class CompteBancaireApp1 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		CompteBancaire compt1 = new CompteBancaire();
+		CompteBancaire compt1 = new CompteBancaire(); // constructeur avec generation et assignation numeroCompte en f°
+														// du compteur de client qui s'incremente à chaque instance
+														// d'objet
 		compt1.afficherDetails();
 		CompteBancaire compt2 = new CompteBancaire();
 		compt2.afficherDetails();
 
-		// tableau de comptes bancaires avec generation de numero de compte (increment à chaque init)
+		// tableau de comptes bancaires avec generation de numero de compte (increment à
+		// chaque init)
 		CompteBancaire[] tablCompt = new CompteBancaire[5];
 		for (int i = 0; i < tablCompt.length; i++) {
 			tablCompt[i] = new CompteBancaire();
