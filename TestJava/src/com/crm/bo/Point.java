fba7a6eb16679176deef11b439a3dd1a4ad2bc53
Joanna <joanna.schmitt@outlook.fr>
2020-02-11 12:26:25 +0100

    point amelio

--- TestJava/src/com/crm/bo/Point.java
+++ TestJava/src/com/crm/bo/Point.java
@@ -35,10 +35,25 @@ public class Point {
 	public void setOrdonne(float ordonne) {
 		this.ordonnee = ordonne;
 	}
+	
+	// affiché coordonnées d'un point
+	public void afficherDetails() {
+		System.out.println("Coordonnées : Abcisse : " + this.abcisse + " / Ordonnée : " + this.ordonnee);
+	}
+
+	
+	/**
+	 * afficher les détails de deux points (ceux de l'objet appelé et ceux de l'objet entré en parametre)
+	 * 
+	 * @param p
+	 */
+	public void afficherDetails(Point p) {
+		System.out.println("Premier point :" + " Abcisse : " + this.abcisse + " / Ordonnée : " + this.ordonnee);
+		System.out.println("Second point :" + " Abcisse : " + p.abcisse + " / Ordonnée : " + p.ordonnee);
+	}
 
 	// racine carré Math.sqrt
 	public float distance(Point p) {
-		return (float) Math
-				.sqrt(Math.pow((this.abcisse - p.abcisse), 2) + Math.pow((this.ordonnee - p.ordonnee), 2));
+		return (float) Math.sqrt(Math.pow((this.abcisse - p.abcisse), 2) + Math.pow((this.ordonnee - p.ordonnee), 2));
 	}
 }
--- TestJava/src/com/crm/boapp/PointApp.java
+++ TestJava/src/com/crm/boapp/PointApp.java
@@ -17,10 +17,28 @@ public class PointApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// constructeur avec parametres
 		Point pointA = new Point(3, 4);
 		Point pointB = new Point(5, 8);
 
-		System.out.println("Distance entre le point a et le point b : " + pointA.distance(pointB));
+		// constructeur sans parametre
+		Point pointC = new Point();
+		System.out.println("Point C");
+
+		// set
+		pointC.setAbcisse(8);
+		pointC.setOrdonne(5);
+		pointC.afficherDetails();
+		System.out.println();
+
+		// get
+		System.out.println("Abcisse Point A : " + pointA.getAbcisse());
+		System.out.println("Ordonnee Point A : " + pointA.getOrdonne());
+		System.out.println();
+
+		pointA.afficherDetails(pointB);
+
+		System.out.println("Distance entre ces deux points : " + pointA.distance(pointB));
 
 	}
 
