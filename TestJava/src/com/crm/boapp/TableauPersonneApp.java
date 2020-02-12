84446115541bb05fda2f98994f63d600d3b7745d
Joanna <joanna.schmitt@outlook.fr>
2020-02-12 09:51:44 +0100

    format

--- TestJava/src/com/crm/boapp/TableauPersonneApp.java
+++ TestJava/src/com/crm/boapp/TableauPersonneApp.java
@@ -26,7 +26,6 @@ public class TableauPersonneApp {
 		nbPersonne = Integer.parseInt(sc.nextLine());
 		Personne[] tabPers = new Personne[nbPersonne]; // Déclaration d'un tableau d'objet de type Personne
 		// construction d'objets de type Personne pour chaque case du tableau
-
 		for (int i = 0; i < tabPers.length; i++) {
 			tabPers[i] = new Personne();
 			System.out.println("\nMembre n° " + (i + 1) + ", veuillez saisir votre Prénom :");
